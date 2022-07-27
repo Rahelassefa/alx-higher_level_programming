234173a01d5128e964a1d02250b677e4bb2756d1
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-07-27 03:21:09 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""
+Module add-integer
+Adds two integer together
 
+"""
+
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return a + b
