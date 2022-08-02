5544831794ac7416a9cc65311ded5ac1cbbcd306
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-08-02 10:31:06 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-""" Module that contains a function that returns the number of lines
-    of a text file
+""" Module that contains a function that writes to a text file
 """
 
 
-def number_of_lines(filename=""):
-    """ Function that reads from a file and prints its number of lines
+def write_file(filename="", text=""):
+    """ Function that writes to a text file
 
     Args:
         filename: filename
+        text: text to write
 
     Raises
         Exception: when the file can be opened
 
     """
-    n_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            n_lines += 1
-    return n_lines
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
