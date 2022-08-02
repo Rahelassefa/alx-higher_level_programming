b4fd95d963786fc4b9eb5420bc60bc8cec6a119a
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-08-02 10:31:41 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-""" Module that returns the dictionary description with a simple
-data structure for a JSON serialization of an object
+""" Module that defines the class Student
 """
 
 
-def class_to_json(obj):
-    """ Function that retuns the dictionary description of an obj """
+class Student:
+    """ Class to create student instances """
 
-    res = {}
-    if hasattr(obj, "__dict__"):
-        res = obj.__dict__.copy()
-    return res
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Method that returns directory description """
+        return self.__dict__.copy()
