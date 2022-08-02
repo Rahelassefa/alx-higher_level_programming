cdc09c3bc72f518514f4aa2f1c20a2cc91533f14
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-08-02 10:17:13 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""Module 11-student.
-Creates a Student class.
+""" Module that defines the class Student
 """
 
 
 class Student:
-    """Class that defines a student.
-    Public attributes:
-        - first_name
-        - last_name
-        - age
-    Public method to_json().
-    """
+    """ Class to create student instances """
 
     def __init__(self, first_name, last_name, age):
-        """Initializes the Student instance."""
-
+        """ Special method to initialize """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation
-        of a Student instance.
-
-        Returns: the dict representation of the instance.
-        """
-
-        return self.__dict__
+        """ Method that returns directory description """
+        return self.__dict__.copy()
