e977fe722b1898f3fc5d5f8f1d789d261fc450f4
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-09-07 11:45:02 +0300

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
-
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- displays all records of a table
+SELECT score, name FROM second_table ORDER BY score DESC;
