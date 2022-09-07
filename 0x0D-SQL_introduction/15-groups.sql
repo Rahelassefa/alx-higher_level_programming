77607d3ac3ff02f64dd438868ab35b5e7de12933
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-09-07 11:53:18 +0300

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,1 +1,3 @@
-
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- groups rows together when they have the same scores and displays the number of occurrences
+SELECT `score`, COUNT(`score`) 'number' FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
