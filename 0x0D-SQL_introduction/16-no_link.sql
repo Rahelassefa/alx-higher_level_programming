f51630fdaef79c85012c84a08eeefac8b45ca402
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-09-07 11:53:50 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,3 @@
-
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists all records in a table except rows without a name value
+SELECT `score`, `name` FROM second_table WHERE `name` IS NOT NULL ORDER BY `score` DESC;
