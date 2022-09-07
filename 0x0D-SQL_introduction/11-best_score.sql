5157893dbdf9adc473753ee1a94ea29e7a6ae65d
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-09-07 11:49:41 +0300

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,1 +1,3 @@
-
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server
+-- lists all records fitting a conditon
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
